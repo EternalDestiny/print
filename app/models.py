5de2eec64329aa85860a7291255a1b6e3241b653
Chanion <384333055@qq.com>
2021-05-13 10:44:19 +0800

    pycharm

--- app/models.py
+++ app/models.py
@@ -69,7 +69,7 @@ class GcodeFile(models.Model):
     gcode_selected = models.BooleanField(default=False)
     gcode_printing = models.BooleanField(default=False)
 
-    # gcode_printer_id = models.CharField(max_length=100, blank=True, null=True, default='no')
+    gcode_printer_id = models.CharField(max_length=100, blank=True, null=True, default='no')
 
     gcode_printfailed = models.BooleanField(default=False)
 
